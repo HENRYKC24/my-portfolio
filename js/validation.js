d18e12c036ae56f4960b1aaabfc1204195b9a70a
Matongo Mulindi <mmatongo_@hotmail.com>
2021-07-29 13:59:18 +0200

    init validation.js file with scaffolding

---
+++ js/validation.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('.contact-me');
+const email = document.querySelector('.email');
+const msgBox = document.querySelector('.error');
+
+email.addEventListener('focus', () => {
+  email.style.outline = 'none';
+});
+
+msgBox.textContent = 'Please make sure the email is all lower case';
+msgBox.style.color = '#fff';
