3c2e50b46bd10e0d6d487652cbe9056743b17488
Matongo Mulindi <mmatongo_@hotmail.com>
2021-07-29 15:41:09 +0200

    add logic to local storage script

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,7 +1,7 @@
 const storeLocally = () => {
-  const name = document.querySelector('#full-name').value;
-  const email = document.querySelector('#your-email').value;
-  const message = document.querySelector('#type-message').value;
+  const name = document.querySelector('.name').value;
+  const email = document.querySelector('.email').value;
+  const message = document.querySelector('.msg').value;
   const formDataObject = {
     name,
     email,
@@ -10,3 +10,16 @@ const storeLocally = () => {
   const stringifiedData = JSON.stringify(formDataObject);
   localStorage.setItem('formDataObject', stringifiedData);
 };
+
+const formDataObject = localStorage.getItem('formDataObject');
+if (formDataObject) {
+  const refinedFormDataObject = JSON.parse(formDataObject);
+  document.querySelector('.name').value = refinedFormDataObject.name;
+  document.querySelector('.email').value = refinedFormDataObject.email;
+  document.querySelector('.msg').value = refinedFormDataObject.message;
+}
+const allFields = Array.from(document.querySelectorAll('.store'));
+allFields.map((field) => {
+  field.addEventListener('keyup', storeLocally);
+  return true;
+});
